3488a8288f574e06b5690aa8f12c2efe82419d4e
mazerati96 <stangstallion@gmail.com>
2026-02-26 00:12:49 -0800

    changes to add a total of 17 dots now for the map

--- js/worldmap.js
+++ js/worldmap.js
@@ -37,6 +37,17 @@ document.addEventListener('DOMContentLoaded', () => {
         region4: { name: 'Region Name', link: 'pages/region4.html' },
         region5: { name: 'Region Name', link: 'pages/region5.html' },
         region6: { name: 'Region Name', link: 'pages/region6.html' },
+        region7: { name: 'Region Name', link: 'pages/region7.html' },
+        region8: { name: 'Region Name', link: 'pages/region8.html' },
+        region9: { name: 'Region Name', link: 'pages/region9.html' },
+        region10: { name: 'Region Name', link: 'pages/region10.html' },
+        region11: { name: 'Region Name', link: 'pages/region11.html' },
+        region12: { name: 'Region Name', link: 'pages/region12.html' },
+        region13: { name: 'Region Name', link: 'pages/region13.html' },
+        region14: { name: 'Region Name', link: 'pages/region14.html' },
+        region15: { name: 'Region Name', link: 'pages/region15.html' },
+        region16: { name: 'Region Name', link: 'pages/region16.html' },
+        region17: { name: 'Region Name', link: 'pages/region17.html' },
     };
 
     // ============================================
